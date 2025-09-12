ebbf77d23ecf4ea6d7c59590fc33337240c4d215
Priyanshu-Singh2002 <142142234+Priyanshu-Singh2002@users.noreply.github.com>
2025-09-12 22:33:38 +0530

    Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
-import { useState } from 'react'
-import Card from './components/Card'
+import { nanoid } from 'nanoid'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [Todo, setTodo] = useState("")
-  const [Todos, setTodoList] = useState([])
+  const [Todo, setTodo] = useState({ id: "", text: "" })
+  const [Todos, setTodos] = useState(() => {
+    let todos = localStorage.getItem("todos")
+   return todos?JSON.parse(todos):[]
+  })
+
+  useEffect(() => {
+    console.log("Saving todos:", Todos)
+    localStorage.setItem("todos", JSON.stringify(Todos))
+  }, [Todos])
+
 
 
   const inputTodos = (e) => {
-    Todo?setTodoList((prev) => [...prev, Todo]):alert("Please enter a task")
-    setTodo('')
+    if (Todo.text.trim() === "") {
+      alert("Please enter a task")
+      return
+    }
+    setTodos((prev) => [...prev, { ...Todo, id: nanoid() }])
+    setTodo({ id: "", text: "" })   // reset properly
   }
 
   const inputDone = (e) => {
-    setTodo(e.target.value)
+    /* setTodo({ ...Todo, id: nanoid(), text:e.target.value})
+    This will keep regenerating a new id on every keypress.
+    The id should only be created wh en adding the todo, not while typing.*/
+    setTodo({ ...Todo, text: e.target.value })
   }
 
+
+  const Edit = (id, newText) => {
+    setTodos((prev) => {
+      return prev.map((Todo) => {
+        return Todo.id === id ? { ...Todo, text: newText } : Todo
+      })
+    })
+  }
+
+  const Delete = (id) => {
+    setTodos(prev => {
+      return prev.filter((todo) => {
+        return todo.id !== id
+      })
+    })
+  }
 
   return (
     <>
@@ -23,13 +56,13 @@ function App() {
         <div className='Main2'>
           <h1>Todo App</h1>
           <div className="input">
-            <input type="text" value={Todo} placeholder='Add a new task' onChange={inputDone} id="Inp1" />
+            <input type="text" value={Todo?.text || ''} placeholder='Add a new task' onChange={inputDone} id="Inp1" />
             <button onClick={inputTodos} id='btn1'>Add</button>
           </div>
           <div className="todolist">
-            <h2 style={{ textAlign: 'start', margin: '15px 0 0 0',color:'purple' }}>Todo List</h2>
-            {Todos.map((todo,index) => {
-              return <Card key={index} todo={todo} />
+            <h2 style={{ textAlign: 'start', margin: '15px 0 0 0', color: 'purple' }}>Todo List</h2>
+            {Todos.map((todo) => {
+              return <Card key={todo.id} Edit={Edit} Delete={Delete} todo={todo} />
             })}
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
 import { FaRegTrashCan } from "react-icons/fa6";
 import { MdModeEdit } from "react-icons/md";
 
 const Card = (props) => {
+
+    const [isEditing, setisEditing] = useState(false)
+
     return (
         <div className='Card'>
             <div className="first">
                 <input type="checkbox" name="" id="" />
-                <span>{props.todo}</span>
+                {isEditing ?<input id='InnerInp' type="text" value={props.todo.text} onChange={(e)=> props.Edit(props.todo.id,e.target.value)} onBlur={() => setisEditing(!isEditing)} autoFocus /> : <span>{props.todo.text}</span>}
             </div>
             <div className="second">
-                <button className='list-button'><MdModeEdit /></button>
-                <button className='list-button'><FaRegTrashCan /></button>
+                <button onClick={() => setisEditing(!isEditing)} className='list-button'><MdModeEdit /></button>
+                <button onClick={() => props.Delete(props.todo.id)} className='list-button'><FaRegTrashCan /></button>
             </div>
         </div>
     )
